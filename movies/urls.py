73d055513aff744c81a6d55eef879195b7240961
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2025-10-09 16:05:17 +0300

    added urls for movies

---
+++ movies/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+
+urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
+    path(
+        'movie/<uuid:movie_id>/',
+        DetailMovieView.as_view(),
+        name='movie-detail'
+        ),
+    path('coming-soon/', SoonPageView.as_view(), name='coming-soon'),
+    path(
+        'genre/<str:genre_name>/',
+        GenreView.as_view(),
+        name='movies-by-genre'
+        )
+]
