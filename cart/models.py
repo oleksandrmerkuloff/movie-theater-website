2e3e6b685d5a471c8637701230003a3fb9b16cb8
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2025-10-10 00:26:43 +0300

    base cart model

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+import uuid
+
+from shop.models import Product
+
+
+User = get_user_model()
+
+
+class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='carts'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'{self.user}\'s cart'
+
+    @property
+    def total(self):
+        return sum(item.price * item.quantity for item in self.items.all())
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(
+        Cart,
+        on_delete=models.CASCADE,
+        related_name='item'
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+    )
+    quantity = models.PositiveIntegerField(
+        default=1,
+        validators=[
+            MinValueValidator(1)
+        ],
+        null=False
+    )
+    price = models.DecimalField(
+        blank=True,
+        max_digits=6,
+        decimal_places=2
+    )
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs) -> None:
+        self.price = self.product.price
+        return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return f'{self.product} in {self.cart}'
+
+    class Meta:
+        ordering = ['-added_at']
+        unique_together = ('cart', 'product',)
