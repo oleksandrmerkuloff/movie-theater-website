c992c8b02d67c75bc9dfa8b6e08f6da18d8433e6
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2025-10-29 23:59:12 +0200

    customize admin panel for the order model

--- orders/admin.py
+++ orders/admin.py
@@ -3,4 +3,11 @@ from django.contrib import admin
 from orders.models import Order
 
 
-admin.site.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'created_at', 'total_text', 'status']
+    list_filter = ['created_at', 'status']
+    sortable_by = ['total_text', 'status']
+    ordering = ['-created_at']
+
+
+admin.site.register(Order, OrderAdmin)
