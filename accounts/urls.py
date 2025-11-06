020e720b74fdd5becad3d44aa08fee7d7a667623
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2025-11-06 13:39:17 +0200

    finish design and views for login/logout/register funtionality and rebuild profile page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 
-from accounts.views import test_profile_view
+from accounts.views import (
+    profile_view,
+    logout_view,
+    login_view,
+    register_view
+)
 
 
 urlpatterns = [
-    path('<uuid:pk>/', test_profile_view, name='test-profile')
+    path('<uuid:pk>/', profile_view, name='test-profile'),
+    path('logout/', logout_view, name='logout'),
+    path('login/', login_view, name='login'),
+    path('register/', register_view, name='register'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,71 @@
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.urls import reverse_lazy
-from django.views.generic import DetailView, UpdateView, DeleteView
-from django.shortcuts import get_object_or_404, render
+from django.contrib.auth import logout, authenticate, login
+from django.shortcuts import get_object_or_404, render, redirect
 
 from orders.models import Order
 from accounts.models import User
 
 
-def test_profile_view(request, pk):
-    orders = Order.objects.filter(user=User)
-    return render(request, 'accounts/profile.html', {'orders': orders})
+def profile_view(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    orders = Order.objects.filter(user=user)
+    return render(request, 'accounts/profile.html', {
+        'user': user,
+        'orders': orders
+        })
 
 
-def profile_view(request):
-    user = request.user
-    profile = user.profile  # Assuming OneToOneField
-    orders = Order.objects.filter(user=user).prefetch_related('items__product')
-    return render(request, 'account/profile.html', {'user': user, 'profile': profile, 'orders': orders})
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+
+def login_view(request):
+    if request.method == "POST":
+        phone = request.POST["phone"]
+        password = request.POST["password"]
+        remember = request.POST.get("remember")
+
+        user = authenticate(request, phone=phone, password=password)
+        if user:
+            login(request, user)
+
+            if not remember:
+                request.session.set_expiry(0)
+
+            return redirect("home")
+
+        return render(request, "accounts/login.html", {"form": {"errors": True}})
+
+    return render(request, "accounts/login.html", {"form": {}})
+
+
+def register_view(request):
+    if request.method == "POST":
+        phone = request.POST.get("phone")
+        fname = request.POST.get("fname")
+        lname = request.POST.get("lname")
+        p1 = request.POST.get("password1")
+        p2 = request.POST.get("password2")
+
+        # Password check
+        if p1 != p2:
+            return render(request, "account/register.html", {
+                "errors": "Passwords do not match."
+            })
+
+        # Username exists
+        if User.objects.filter(phone=phone).exists():
+            return render(request, "account/register.html", {
+                "errors": "User already exists."
+            })
+
+        user = User.objects.create_user(
+            phone=phone,
+            fname=fname,
+            lname=lname,
+            password=p1
+            )
+        login(request, user)
+        return redirect("home")
+
+    return render(request, "accounts/register.html")
